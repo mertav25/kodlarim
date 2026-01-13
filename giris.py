4e34116369b2f56326c418d0d20de0e94306b4dc
mertav25 <148698079+mertav25@users.noreply.github.com>
2026-01-13 14:24:38 +0300

    Add introductory comments to giris.py

---
+++ giris.py
@@ -0,0 +1,14 @@
+#Python nasıl çalışılır?
+"""Source kod öncelikle compiler ile bytecoda çevrilir.Bu byte coda kütüphaneler ve inputlar
+eklenir ve virtual machine altında çalıştırılır.Bu işlemler interepterde gerçekleşir,outputa çevrilir
+
+"""
+
+#Pythonda kullanılan data typler
+#text için str yani string kullanılır
+#numeric için int float complex veri tipleri kullanılır
+#sequence için list,tupple,range
+#mapping için sözlük
+#set için set,frozenset
+#Boolean için bool
+#Binary için bytes,bytearray,memoryview
